8ae9d7d0fcd76bcea27493f104c6f27596c1e618
Stephen Rajesh <smr.itprof@gmail.com>
2020-11-24 16:00:07 +0000

    Nexonone

---
+++ pyth/oops4.py
@@ -0,0 +1,19 @@
+import math
+
+def calcCircumference(radius):
+    return math.pi * 2 * radius 
+
+class Circle:
+    pass
+
+circle1 = Circle()
+
+circle1.radius = 4.2
+
+print(circle1.radius)
+
+circle2 = Circle()
+circle2.radius = 3.9
+
+print(circle2.radius)
+
